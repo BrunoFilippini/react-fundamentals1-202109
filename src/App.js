50133f0746a9c60fe85849bcff513668666558c7
Bruno Filippini <brunofilippini94@gmail.com>
2022-02-03 18:24:40 -0300

    first challenge done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-
+import { IdCard } from './components/IdCard'
 
 function App() {
   return (
     <>
-      
+
+    <IdCard
+      fullName='John Doe'
+      gender='male'
+      birthday="1992-07-14"
+      picture="https://randomuser.me/api/portraits/men/44.jpg"
+     />
+
+    <IdCard 
+      fullName='Obrien Delores'
+      gender='female'
+      birth="1988-05-11"
+      picture="https://randomuser.me/api/portraits/women/44.jpg"
+    />
 
     </>
   );
---
+++ src/components/IdCard/index.js
@@ -0,0 +1,21 @@
+import styles from "./styles.module.css";
+
+
+
+export function IdCard(props) {
+  return (
+    <div className={styles.card}>
+       
+        <div>
+
+          <p><span>Full Name:</span>{props.fullName}</p>
+          <p><span>Gender:</span> {props.gender}</p>
+          <p><span>Birthday:</span> {props.birth} {props.birthday}</p>
+
+          <img src={props.picture} alt={props.alt} />
+
+        </div>
+
+    </div>
+  );
+}
